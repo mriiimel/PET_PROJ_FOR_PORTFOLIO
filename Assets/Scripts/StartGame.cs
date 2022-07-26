c6ec704a994c3ad063007280eb8ed0547e8321e6
mriiimel <60065827+mriiimel@users.noreply.github.com>
2022-07-26 14:11:41 +0300

    Work with UI
    Work with UI ,script for UI

--- Assets/Scripts/StartGame.cs
+++ Assets/Scripts/StartGame.cs
@@ -3,11 +3,30 @@ using UnityEngine;
 
 public class StartGame : MonoBehaviour
 {
-    
+    [SerializeField] private Canvas _settingsCanvas;
+    private void Awake()
+    {
+        _settingsCanvas.enabled = false;
+    }
+    private void Update()
+    {
+        if (_settingsCanvas.isActiveAndEnabled == true)
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                _settingsCanvas.enabled = false;
+            }
+        }
+    }
     public void StartGameBTN()
     {
         SceneManager.LoadScene("MainScene");
     }
-
+    public void SettigsBtn()
+    {
+        _settingsCanvas.enabled = true;
+    }
     
+
+
 }
